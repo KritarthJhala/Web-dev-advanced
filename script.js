467db16a0f9e7d37d8ff8d1b31213904fbbb0a93
Kritarth <kritarthjhala12@gmail.com>
2026-03-06 16:14:14 +0530

    Async-Await

--- script.js
+++ script.js
@@ -389,15 +389,17 @@
 // para.innerText="This is a paragraph"
 
 
-//Callback hell
-// function getData(dataId, getNextData){
-//     setTimeout(()=>{
-//         console.log("data:", dataId);
-//         if (getNextData){
-//             getNextData();
-//         }
-//     },2000);
-// }
+//Function GetData
+function getData(dataId, getNextData){
+    setTimeout(()=>{
+        console.log("data:", dataId);
+        if (getNextData){
+            getNextData();
+        }
+    },2000);
+}
+
+//Callback Hell
 
 // getData(1,()=>{
 //     console.log("Getting data 2 ....");
@@ -412,25 +414,54 @@
 
 
 //Promises
-function asyncFunc1(){
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            console.log("data1");
-            resolve("success");
+// function asyncFunc1(){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             console.log("data1");
+//             resolve("success");
 
-        },4000);
-    });
+//         },4000);
+//     });
+// }
+// function asyncFunc2(){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             console.log("data2");
+//             resolve("success");
+//         },4000);
+//     });
+// }
+// console.log("fetching data1....");
+// asyncFunc1().then((res)=>{
+//     console.log("fetching data2....");
+//     asyncFunc2().then((res)=>{});
+// });
+
+//Promise chain
+// console.log("getting data 1....")
+// getData(1)
+//     .then((res)=>{
+//         console.log("getting data 2....");
+//         return getData(2);
+//     })
+//     .then((res)=>{
+//         console.log("getting data 3....");
+//         return getData(3);
+//     })
+//     .then((res)=>{
+//         console.log(res);
+//     });
+
+//Async-await
+async function getAllData(){
+    console.log("getting data 1....");
+    await getData(1);
+    console.log("getting data 2....");
+    await getData(2);
+    console.log("getting data 3....");
+    await getData(3);
+    console.log("getting data 4....");
+    await getData(4);
+    console.log("getting data 5....");
+    await getData(5);
 }
-function asyncFunc2(){
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            console.log("data2");
-            resolve("success");
-        },4000);
-    });
-}
-console.log("fetching data1....");
-asyncFunc1().then((res)=>{
-    console.log("fetching data2....");
-    asyncFunc2().then((res)=>{});
-});
